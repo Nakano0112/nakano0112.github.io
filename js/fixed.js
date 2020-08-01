d5789dcd95c1ed12b2b41cc171c77b56ffdb4e4a
Nakano0112 <shuya112@icloud.com>
2020-08-01 17:48:54 +0900

    new

---
+++ js/fixed.js
@@ -0,0 +1,11 @@
+$(function() {
+    var menu = $('#menu'),
+    offset = menu.offset();
+  $(window).scroll(function () {
+    if($(window).scrollTop() > offset.top) {
+      menu.addClass('fixed');
+    } else {
+      menu.removeClass('fixed');
+      }
+    });
+  });
